6e577aaac0dc46a2ca829f7b959e5c99968c3285
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-08-27 00:05:30 +0100

    add extension logic
    unhide old messages by removing the bluring class

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-console.log("testing the extension");
+function unblurMessages() {
+  const blurredMessages = document.querySelectorAll(
+    ".c-message_kit__hidden_message_blur"
+  );
+  blurredMessages.forEach((message) => {
+    message.classList.remove("c-message_kit__hidden_message_blur");
+  });
+}
+
+// run function when the page loads
+unblurMessages();
+
+// observe the DOM for changes and unblur new ones
+const observer = new MutationObserver(() => unblurMessages());
+observer.observe(document.body, { childList: true, subtree: true });
