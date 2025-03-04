9db9d9b8b88ffa1c4a33883d8589480273b535e4
Christopher Nash <cnash6@gmail.com>
2025-03-04 12:49:32 -0500

    New function to remove c-truncate and c-truncate--break_words classes

--- content.js
+++ content.js
@@ -4,6 +4,14 @@ function unblurMessages() {
   );
   blurredMessages.forEach((message) => {
     message.classList.remove("c-message_kit__hidden_message_blur");
+  });
+}
+
+function untruncateMessages() {
+  const truncatedMessages = document.querySelectorAll(
+    ".c-truncate, .c-truncate--break_words"
+  );
+  truncatedMessages.forEach((message) => {
     message.classList.remove("c-truncate");
     message.classList.remove("c-truncate--break_words");
   });
@@ -18,11 +26,13 @@ function removeLimitedHistoryBanner() {
 
 // run functions when the page loads
 unblurMessages();
+untruncateMessages();
 removeLimitedHistoryBanner();
 
 // observe the DOM for changes and apply functions when applicable
 const observer = new MutationObserver(() => {
   unblurMessages();
+  untruncateMessages();
   removeLimitedHistoryBanner();
 });
 
